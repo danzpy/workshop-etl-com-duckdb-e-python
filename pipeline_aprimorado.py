c8ded35d8efa8def882844f3063eae2f550f12a3
Dan <daniel.leonssio@hotmail.com>
2024-07-27 15:50:22 -0300

    Tendando descobrir o motivo da variavel DATABASE_URL não estar sendo encontrada

--- pipeline_aprimorado.py
+++ pipeline_aprimorado.py
@@ -65,13 +65,14 @@ def pipeline() -> list:
 
     URL_PASTA: Path = os.getenv("URL_PASTA")
     DIRETORIO_LOCAL: Path = './pasta_gdown'
+    DATABASE_URL = os.getenv("DATABASE_URL")
     
-    # try:
-    #     logs.append(f"Baixando arquivos do Google Drive para o diretório {DIRETORIO_LOCAL}.")
-    #     pp.baixar_arquivos(URL_PASTA, DIRETORIO_LOCAL)
-    # except Exception as e:
-    #     logs.append(f'Não foi possível se conectar ao GoogleDrive.')
-    #     return logs
+    try:
+        logs.append(f"Baixando arquivos do Google Drive para o diretório {DIRETORIO_LOCAL}.")
+        pp.baixar_arquivos(URL_PASTA, DIRETORIO_LOCAL)
+    except Exception as e:
+        logs.append(f'Não foi possível se conectar ao GoogleDrive: {str(e)}')
+        return logs
 
     con = conectar_banco()
     inicializar_tabela(con)
